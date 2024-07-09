6706d44105794ab91660bda2dde2aa5a159c476d
misung.dev <misung.dev@gmail.com>
2024-07-10 02:26:40 +0900

    feat: 헤더 내 링크 구현 및 router 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import * as S from './Header.style';
+import { Link } from 'react-router-dom';
 import logo from '../../icons/logo.svg';
 
 export default function Header() {
   return (
     <S.Header>
-      <S.LogoWrapper>
-        <img src={logo} alt='로고 이미지' />
-      </S.LogoWrapper>
+      <Link to='/home'>
+        <S.LogoWrapper>
+          <img src={logo} alt='로고 이미지' />
+        </S.LogoWrapper>
+      </Link>
     </S.Header>
   );
 }
