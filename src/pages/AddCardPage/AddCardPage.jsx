031d11b2f7e8f33b69815c05a7c3be6ca0312ab9
misung.dev <misung.dev@gmail.com>
2024-07-09 21:04:14 +0900

    feat: 검색창, 타이틀 구현

--- src/pages/AddCardPage/AddCardPage.jsx
+++ src/pages/AddCardPage/AddCardPage.jsx
@@ -1,5 +1,22 @@
 import * as S from './AddCardPage.style';
+import magnifyingGlassIcon from '../../icons/icon-magnifying-glass.svg';
+import more from '../../icons/icon-more.svg';
+import arrowRight from '../../icons/icon-arrow-right.svg';
 
 export default function AddCardPage() {
-  return <S.Container></S.Container>;
+  return (
+    <S.AddCardPage>
+      <S.SearchBar>
+        <S.SearchIcon>
+          <img src={magnifyingGlassIcon} alt='돋보기' />
+        </S.SearchIcon>
+        <S.SearchInput placeholder='명함을 검색해주세요' />
+      </S.SearchBar>
+
+      <S.TitleContainer>
+        <S.Title>명함 추가하기</S.Title>
+        <S.Subtitle>사진을 첨부하거나 직접 입력하여 명함 추가하기</S.Subtitle>
+      </S.TitleContainer>
+    </S.AddCardPage>
+  );
 }
--- src/pages/AddCardPage/AddCardPage.style.jsx
+++ src/pages/AddCardPage/AddCardPage.style.jsx
@@ -1,7 +1,63 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div`
-  border: 1px solid black;
-  height: 350px;
-  background-color: gray;
+export const AddCardPage = styled.div`
+  padding: 20px;
+`;
+
+// 검색창
+export const SearchBar = styled.div`
+  display: flex;
+  height: 36px;
+  margin-bottom: 16px;
+  padding: 10px 18px;
+  align-items: center;
+  flex-shrink: 0;
+  border-radius: 10px;
+  background: #efefef;
+  gap: 10px;
+`;
+
+export const SearchIcon = styled.div`
+  width: 10px;
+  height: 14px;
+  transform: rotate(-47.353deg);
+  flex-shrink: 0;
+  fill: var(--grey2, #949494);
+`;
+
+export const SearchInput = styled.input`
+  border: none;
+  outline: none;
+  background: transparent;
+  width: 100%;
+  color: var(--grey3, #555);
+  font-size: 9px;
+  line-height: 150%;
+  letter-spacing: -0.5px;
+
+  &::placeholder {
+    color: #999;
+  }
+`;
+
+// 타이틀
+export const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+export const Title = styled.h1`
+  color: var(--grey4, #1a1a1a);
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 150%;
+  letter-spacing: -0.5px;
+`;
+
+export const Subtitle = styled.p`
+  color: var(--grey3, #555);
+  font-size: 9px;
+  line-height: 150%;
+  letter-spacing: -0.5px;
 `;
