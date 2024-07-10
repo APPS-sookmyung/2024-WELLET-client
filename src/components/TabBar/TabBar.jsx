11dabbbe4e547e91cdd4c90d0567a88659d9b8ca
misung.dev <misung.dev@gmail.com>
2024-07-10 21:45:17 +0900

    feat: 탭바 활성화시 아이콘 색상 변경

--- src/components/TabBar/TabBar.jsx
+++ src/components/TabBar/TabBar.jsx
@@ -3,20 +3,43 @@ import { useLocation } from 'react-router-dom';
 import * as S from './TabBar.style';
 import { Link } from 'react-router-dom';
 import homeLogo from '../../icons/icon-nav-home.svg';
+import homeActiveLogo from '../../icons/icon-nav-home-active.svg';
 import addCardLogo from '../../icons/icon-nav-add-card.svg';
+import addCardActiveLogo from '../../icons/icon-nav-add-card-active.svg';
 import viewCardLogo from '../../icons/icon-nav-view-card.svg';
+import viewCardActiveLogo from '../../icons/icon-nav-view-card-active.svg';
 import mypageLogo from '../../icons/icon-nav-mypage.svg';
+import mypageActiveLogo from '../../icons/icon-nav-mypage-active.svg';
 
 const tabItems = [
-  { path: '/home', label: '홈', logo: homeLogo, alt: '홈' },
+  {
+    path: '/home',
+    label: '홈',
+    logo: homeLogo,
+    activeLogo: homeActiveLogo,
+    alt: '홈',
+  },
   {
     path: '/card/add',
     label: '명함 추가',
     logo: addCardLogo,
+    activeLogo: addCardActiveLogo,
     alt: '명함 추가',
   },
-  { path: '/card', label: '명함 보기', logo: viewCardLogo, alt: '명함 보기' },
-  { path: '/mypage', label: '마이페이지', logo: mypageLogo, alt: '마이페이지' },
+  {
+    path: '/card',
+    label: '명함 보기',
+    logo: viewCardLogo,
+    activeLogo: viewCardActiveLogo,
+    alt: '명함 보기',
+  },
+  {
+    path: '/mypage',
+    label: '마이페이지',
+    logo: mypageLogo,
+    activeLogo: mypageActiveLogo,
+    alt: '마이페이지',
+  },
 ];
 
 export default function TabBar() {
@@ -25,16 +48,19 @@ export default function TabBar() {
 
   return (
     <S.TabBar>
-      {tabItems.map(({ path, label, logo, alt }) => (
-        <S.Icon key={path} isActive={currentPath === '/home'}>
-          <Link to={path}>
-            <S.IconImg isActive={currentPath === path}>
-              <img src={logo} alt={alt} />
-            </S.IconImg>
-            <S.Text isActive={currentPath === path}>{label}</S.Text>
-          </Link>
-        </S.Icon>
-      ))}
+      {tabItems.map(({ path, label, logo, activeLogo, alt }) => {
+        const isActive = currentPath === path;
+        return (
+          <S.Icon key={path} isActive={isActive}>
+            <Link to={path}>
+              <S.IconImg isActive={isActive}>
+                <img src={isActive ? activeLogo : logo} alt={alt} />
+              </S.IconImg>
+              <S.Text isActive={isActive}>{label}</S.Text>
+            </Link>
+          </S.Icon>
+        );
+      })}
     </S.TabBar>
   );
 }
