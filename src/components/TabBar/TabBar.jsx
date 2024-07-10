d864d797ba78fd3d6ad0373c4381b592fd9cbeb1
misung.dev <misung.dev@gmail.com>
2024-07-10 20:09:46 +0900

    fix: 검색창 내 돋보기 아이콘 수정

--- src/components/TabBar/TabBar.jsx
+++ src/components/TabBar/TabBar.jsx
@@ -7,45 +7,34 @@ import addCardLogo from '../../icons/icon-nav-add-card.svg';
 import viewCardLogo from '../../icons/icon-nav-view-card.svg';
 import mypageLogo from '../../icons/icon-nav-mypage.svg';
 
+const tabItems = [
+  { path: '/home', label: '홈', logo: homeLogo, alt: '홈' },
+  {
+    path: '/card/add',
+    label: '명함 추가',
+    logo: addCardLogo,
+    alt: '명함 추가',
+  },
+  { path: '/card', label: '명함 보기', logo: viewCardLogo, alt: '명함 보기' },
+  { path: '/mypage', label: '마이페이지', logo: mypageLogo, alt: '마이페이지' },
+];
+
 export default function TabBar() {
   const location = useLocation();
+  const currentPath = location.pathname;
 
   return (
     <S.TabBar>
-      <S.Icon isActive={location.pathname === '/home'}>
-        <Link to='/home'>
-          <S.IconImg isActive={location.pathname === '/home'}>
-            <img src={homeLogo} alt='홈' />
-          </S.IconImg>
-          <S.Text isActive={location.pathname === '/home'}>홈</S.Text>
-        </Link>
-      </S.Icon>
-      <S.Icon isActive={location.pathname === '/card/add'}>
-        <Link to='/card/add'>
-          <S.IconImg isActive={location.pathname === '/card/add'}>
-            <img src={addCardLogo} alt='명함 추가' />
-          </S.IconImg>
-          <S.Text isActive={location.pathname === '/card/add'}>
-            명함 추가
-          </S.Text>
-        </Link>
-      </S.Icon>
-      <S.Icon isActive={location.pathname === '/card'}>
-        <Link to='/card'>
-          <S.IconImg isActive={location.pathname === '/card'}>
-            <img src={viewCardLogo} alt='명함 보기' />
-          </S.IconImg>
-          <S.Text isActive={location.pathname === '/card'}>명함 보기</S.Text>
-        </Link>
-      </S.Icon>
-      <S.Icon isActive={location.pathname === '/mypage'}>
-        <Link to='/mypage'>
-          <S.IconImg isActive={location.pathname === '/mypage'}>
-            <img src={mypageLogo} alt='마이페이지' />
-          </S.IconImg>
-          <S.Text isActive={location.pathname === '/mypage'}>마이페이지</S.Text>
-        </Link>
-      </S.Icon>
+      {tabItems.map(({ path, label, logo, alt }) => (
+        <S.Icon key={path} isActive={currentPath === '/home'}>
+          <Link to={path}>
+            <S.IconImg isActive={currentPath === path}>
+              <img src={logo} alt={alt} />
+            </S.IconImg>
+            <S.Text isActive={currentPath === path}>{label}</S.Text>
+          </Link>
+        </S.Icon>
+      ))}
     </S.TabBar>
   );
 }
