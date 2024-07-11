06a34cf0b03449e67151aaeebf3f8b2e985ca9b3
kimhyerims <hyerim218@naver.com>
2024-07-11 20:25:56 +0900

    Feat: mypage 구현

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,14 +1,53 @@
+import React, { useState } from 'react';
 import * as S from './MyPage.style';
-import { Header, TabBar } from '../../components';
+import messageicon from '../../icons/icon-message.svg';
+import callicon from '../../icons/icon-call.svg';
+import mailicon from '../../icons/icon-mail.svg';
+import shareicon from '../../icons/icon-share.svg';
+import galleryicon from '../../icons/icon-gallery.svg';
+import dot3icon from '../../icons/icon-dot3.svg';
+
 
 export default function MyPage() {
+  const [myInfo, setMyInfo] = useState({
+    name: '김은지',
+    title: 'Web Engineer / 개발실',
+    company: 'WELLET Corp.',
+  });
+
+  const [myContact, setMyContact] = useState({
+    phone: '010-1234-5678',
+    email: 'email@welletapp.co.kr',
+    tel: '81-2-222-3456',
+    address: '서울시 강남구 테헤란로 134, 5-6층 (역삼동, 포스크타워 역삼)',
+  });
   return (
     <>
-      <S.MyPage>
-        <Header />
-        <S.Container>마이페이지</S.Container>
-      </S.MyPage>
-      <TabBar />
+    <S.Mybar1>
+      <S.barbox1>내 명함</S.barbox1>
+      <S.EditIcon>편집하기</S.EditIcon>
+      <S.ProfilePic/><S.IconBarContainer><img src={shareicon}/><img src={galleryicon}/><img src={dot3icon}/></S.IconBarContainer>
+      <S.barbox2>{myInfo.name}</S.barbox2>
+      <S.subbar/>
+    </S.Mybar1>
+    <S.Mybar2>
+    <S.Infobar>내 정보</S.Infobar>
+    <S.Container1>
+      <div>
+        <div><S.Font1>성명</S.Font1><S.Font2>{myInfo.company}</S.Font2><br /></div>
+        <div><S.Font1>직무/소속</S.Font1><S.Font2>{myInfo.title}</S.Font2></div>
+      </div>
+    </S.Container1>
+    <S.Infobar>내 연락처</S.Infobar>
+    <S.Container2>
+    <div>
+        <div><S.Font1>휴대폰</S.Font1><S.Font2>{myContact.phone}<img src={messageicon}/><img src={callicon}/></S.Font2></div>
+        <div><S.Font1>이메일</S.Font1><S.Font2>{myContact.email}<img src={mailicon}/></S.Font2></div>
+        <div><S.Font1>유선전화</S.Font1><S.Font2>{myContact.tel}<img src={callicon}/></S.Font2></div>
+        <div><S.Font1>주소</S.Font1><S.Font2>{myContact.address}</S.Font2></div>
+      </div>
+    </S.Container2>
+    </S.Mybar2>
     </>
   );
-}
+};
--- src/pages/MyPage/MyPage.style.jsx
+++ src/pages/MyPage/MyPage.style.jsx
@@ -1,9 +1,185 @@
 import styled from '@emotion/styled';
 
-export const MyPage = styled.div`
+export const Container1 = styled.div`
+  width: 728px;
+  height: 99.049px;
+  flex-shrink: 0;
+  border-radius: 10px;
+  background: #f4f4f9;
+`;
+
+export const Container2 = styled.div`
+  width: 728px;
+  height: 185.979px;
+  flex-shrink: 0;
+  border-radius: 10px;
+  background: #f4f4f9;
+`;
+
+export const Mybar1 = styled.div`
+  width: 100%;
+  height: 244px;
+  flex-shrink: 0;
+  border-radius: 0px 0px 20px 20px;
+  background: linear-gradient(111deg, #2d29ff 35.96%, #721cff 116.82%);
+  position: relative;
   padding: 20px;
 `;
 
-export const Container = styled.div`
-  background-color: gray;
+export const Mybar2 = styled.div`
+  width: 100%;
+  height: 724px;
+  background: #fff;
+  padding: 20px;
+  box-sizing: border-box;
+`;
+
+export const barbox1 = styled.div`
+  color: var(--white, #fff);
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 150%;
+  letter-spacing: -0.5px;
+  text-align: center;
+  position: absolute;
+  top: 30px; 
+  left: 50%;
+  transform: translateX(-50%); 
+`;
+
+export const barbox2 = styled.div`
+  color: var(--white, #fff);
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 150%; 
+  letter-spacing: -0.5px;
+  text-align: center;
+  position: absolute; 
+  top: 190px; 
+  left: 50%; 
+  transform: translateX(-50%); 
+`;
+
+export const subbar = styled.div`
+  width: 71.766px;
+  height: 4.664px;
+  flex-shrink: 0;
+  border-radius: 4px;
+  background: #8776ff;
+  position: absolute; /* 위치를 절대값으로 변경 */
+  top: 230px; /* 위에서부터 230px */
+  left: 50%; /* 가운데 정렬 */
+  transform: translateX(-50%); /* 가운데 정렬 */
+`;
+
+export const EditIcon = styled.div`
+  display: inline-flex;
+  padding: 4px 10px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 8px;
+  border-radius: 10px;
+  background: var(--color-1, #ebf3fe);
+  color: var(--primary, #2d29ff);
+  font-size: 9px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%; /* 13.5px */
+  letter-spacing: -0.5px;
+  position: absolute; /* 위치를 절대값으로 변경 */
+  top: 20px; /* 위에서부터 20px */
+  right: 20px; /* 오른쪽에서 20px */
+  height: 24px; /* barbox와 동일한 높이 */
+`;
+
+export const ProfilePic = styled.img`
+  width: 97px;
+  height: 97px;
+  flex-shrink: 0;
+  border-radius: 97px;
+  position: absolute;
+  top: 83px; /* 위에서부터 83px */
+  left: 50%; /* 가운데 정렬 */
+  transform: translateX(-50%); /* 가운데 정렬 */
+`;
+
+export const Infobar = styled.div`
+  color: var(--grey4, #1a1a1a);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%; /* 21px */
+  letter-spacing: -0.5px;
+`;
+
+export const Font1 = styled.div`
+  color: var(--grey2, #949494);
+  font-size: 9px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%; /* 13.5px */
+  letter-spacing: -0.5px;
+`;
+
+export const Font2 = styled.div`
+  color: var(--grey4, #1a1a1a);
+  font-size: 11px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%; /* 16.5px */
+  letter-spacing: -0.5px;
+`;
+
+export const IconBarContainer = styled.div`
+  position: absolute;
+  top: 84px;
+  right: 20px;
+  display: flex;
+  gap: 16px;
+  padding: 2px;
+`;
+
+export const messageIcon = styled.img`
+  width: 15.831px;
+  height: 14.392px;
+  position: absolute; /* 위치를 절대값으로 변경 */
+  top: 466px; /* 위에서부터 466px */
+  right: 70px;
+`;
+
+export const callIcon = styled.img`
+  width: 16px;
+  height: 18px;
+  position: absolute;
+  top: 466px; 
+  right: 50px;
+`;
+
+export const mailIcon = styled.img`
+  width: 18px;
+  height: 13px;
+  position: absolute;
+  top: 466px; /* 여기에 적절한 위치 값을 설정하세요 */
+  right: 30px;
+`;
+
+export const shareicon = styled.img`
+  width: 11.373px;
+  height: 17.234px;
+  flex-shrink: 0;
+  fill: #fff;
+`;
+
+export const galleryicon = styled.img`
+  width: 18.172px;
+  height: 18.172px;
+  flex-shrink: 0;
+`;
+
+export const dot3icon = styled.img`
+  width: 2.4px;
+  height: 10.453px;
+  flex-shrink: 0;
 `;
