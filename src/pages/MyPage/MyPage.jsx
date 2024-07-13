2aecf16851d21ecb994283baae67d0110f89046c
kimhyerims <hyerim218@naver.com>
2024-07-13 21:58:34 +0900

    feat: 편집하기 경로 설정

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -6,7 +6,7 @@ import mailicon from '../../icons/icon-mail.svg';
 import shareicon from '../../icons/icon-share.svg';
 import galleryicon from '../../icons/icon-gallery.svg';
 import dot3icon from '../../icons/icon-dot3.svg';
-
+import { useNavigate } from 'react-router-dom';
 
 export default function MyPage() {
   const [myInfo, setMyInfo] = useState({
@@ -21,45 +21,83 @@ export default function MyPage() {
     tel: '81-2-222-3456',
     address: '서울시 강남구 테헤란로 134, 5-6층 (역삼동, 포스크타워 역삼)',
   });
+
+  const navigate = useNavigate();
+  const EditClick = () => {
+    navigate('/mypage/edit');
+  };
   return (
     <>
-    <S.Mybar1>
-      <S.barbox1>내 명함</S.barbox1>
-      <S.EditIcon>편집하기</S.EditIcon>
-      <S.ProfilePic/><S.IconBarContainer><img src={shareicon}/><img src={galleryicon}/><img src={dot3icon}/></S.IconBarContainer>
-      <S.barbox2>{myInfo.name}</S.barbox2>
-      <S.subbar/>
-    </S.Mybar1>
-    <S.Mybar2>
-    <S.Infobar>내 정보</S.Infobar>
-    <S.Container1>
-      <div>
-        <div><S.Font1>성명</S.Font1><S.Font2>{myInfo.company}</S.Font2><br /></div>
-        <div><S.Font1>직무/소속</S.Font1><S.Font2>{myInfo.title}</S.Font2></div>
-      </div>
-    </S.Container1>
-    <S.Infobar>내 연락처</S.Infobar>
-    <S.Container2>
-      <div style={{display:'flex',justifyContent: 'space-between'}}>
-        <div><S.Font1>휴대폰</S.Font1><S.Font2>{myContact.phone}</S.Font2></div>
-        <div><S.IconBox><img src={messageicon}/><img src={callicon}/></S.IconBox></div>
-      </div>
-      <br/>
-      <div style={{display:'flex',justifyContent: 'space-between'}}>
-        <div><S.Font1>이메일</S.Font1><S.Font2>{myContact.email}</S.Font2></div>
-        <div><img src={mailicon}/></div>
-      </div>
-      <br/>
-      <div style={{display:'flex',justifyContent: 'space-between'}}>
-        <div><S.Font1>유선전화</S.Font1><S.Font2>{myContact.tel}</S.Font2></div>
-        <div><img src={callicon}/></div>
-      </div>
-      <br/>
-      <div>
-        <div><S.Font1>주소</S.Font1><S.Font2>{myContact.address}</S.Font2></div>
-      </div>
-    </S.Container2>
-    </S.Mybar2>
+      <S.Mybar1>
+        <S.barbox1>내 명함</S.barbox1>
+        <S.EditIcon onClick={EditClick}>편집하기</S.EditIcon>
+        <S.ProfilePic />
+        <S.IconBarContainer>
+          <img src={shareicon} />
+          <img src={galleryicon} />
+          <img src={dot3icon} />
+        </S.IconBarContainer>
+        <S.barbox2>{myInfo.name}</S.barbox2>
+        <S.subbar />
+      </S.Mybar1>
+      <S.Mybar2>
+        <S.Infobar>내 정보</S.Infobar>
+        <S.Container1>
+          <div>
+            <div>
+              <S.Font1>성명</S.Font1>
+              <S.Font2>{myInfo.company}</S.Font2>
+              <br />
+            </div>
+            <div>
+              <S.Font1>직무/소속</S.Font1>
+              <S.Font2>{myInfo.title}</S.Font2>
+            </div>
+          </div>
+        </S.Container1>
+        <S.Infobar>내 연락처</S.Infobar>
+        <S.Container2>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div>
+              <S.Font1>휴대폰</S.Font1>
+              <S.Font2>{myContact.phone}</S.Font2>
+            </div>
+            <div>
+              <S.IconBox>
+                <img src={messageicon} />
+                <img src={callicon} />
+              </S.IconBox>
+            </div>
+          </div>
+          <br />
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div>
+              <S.Font1>이메일</S.Font1>
+              <S.Font2>{myContact.email}</S.Font2>
+            </div>
+            <div>
+              <img src={mailicon} />
+            </div>
+          </div>
+          <br />
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div>
+              <S.Font1>유선전화</S.Font1>
+              <S.Font2>{myContact.tel}</S.Font2>
+            </div>
+            <div>
+              <img src={callicon} />
+            </div>
+          </div>
+          <br />
+          <div>
+            <div>
+              <S.Font1>주소</S.Font1>
+              <S.Font2>{myContact.address}</S.Font2>
+            </div>
+          </div>
+        </S.Container2>
+      </S.Mybar2>
     </>
   );
-};
+}
