04942795bfc6e81684e767054e9fca70c11abeb0
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-07-16 20:47:06 +0900

    Feat: 아이콘 컴포넌트 방식으로 변경

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import * as S from './Header.style';
 import { Link } from 'react-router-dom';
-import logo from '../../icons/logo.svg';
+// import logo from '../../icons/logo.svg';
+import Icon from '../Icon/Icon';
 
-export default function Header() {
+export default function Header({ fill }) {
   return (
     <S.Header>
       <Link to='/home'>
         <S.LogoWrapper>
-          <img src={logo} alt='로고 이미지' />
+          <Icon id='logo' fill={fill} />
         </S.LogoWrapper>
       </Link>
     </S.Header>
