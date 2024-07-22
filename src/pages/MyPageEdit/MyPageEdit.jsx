5d88ce98aaf12b78cccb8d46d7e4716efd3daaad
kimhyerims <hyerim218@naver.com>
2024-07-22 22:42:00 +0900

    feat: mypage-edit 구현

--- src/pages/MyPageEdit/MyPageEdit.jsx
+++ src/pages/MyPageEdit/MyPageEdit.jsx
@@ -1,16 +1,142 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './MyPageEdit.style';
 import logoicon from '../../icons/logo.svg';
-//import { myInfo, myContact } from 'MyPage.jsx';
+import arrowicon from '../../icons/icon-arrow.svg';
+import arrowdownicon from '../../icons/icon-arrow-down.svg';
+import pencilicon from '../../icons/icon-pencil2.svg';
+import galleryicon from '../../icons/icon-gallery.svg';
+import { useNavigate } from 'react-router-dom';
+
 export default function MyPageEdit() {
+  const [myInfo, setMyInfo] = useState({
+    name: '김은지',
+    team: '개발팀',
+    job: 'Web Engineer',
+    company: 'WELLET Corp.',
+  });
+
+  const [myContact, setMyContact] = useState({
+    phone: '010-1234-5678',
+    email: 'email@welletapp.co.kr',
+    tel: '81-2-222-3456',
+    address: '서울시 강남구 테헤란로 134, 5-6층 (역삼동, 포스크타워 역삼)',
+  });
+
+  const navigate = useNavigate();
+
+  const MyPageClick = () => {
+    navigate('/mypage');
+  };
+
   return (
     <>
       <S.Header>
+        <S.arrowicon onClick={MyPageClick}>
+          <img src={arrowicon} />
+        </S.arrowicon>
         <S.welletlogo>
           <img src={logoicon} />
         </S.welletlogo>
         <S.EditIcon>편집완료</S.EditIcon>
       </S.Header>
+      <S.Body>
+        <S.PicContainer>
+          <S.ProfilePic />
+          <S.galleryicon>
+            <img src={galleryicon} />
+          </S.galleryicon>
+        </S.PicContainer>
+        <S.EditInfoContainer>
+          <S.EditName>{myInfo.name}</S.EditName>
+          <S.EditInfo>
+            {myInfo.team}{' '}
+            <S.arrowdownicon>
+              <img src={arrowdownicon} />
+            </S.arrowdownicon>{' '}
+            / {myInfo.job}
+            <S.arrowdownicon>
+              <img src={arrowdownicon} />
+            </S.arrowdownicon>
+          </S.EditInfo>
+          <S.EditCompany>{myInfo.company}</S.EditCompany>
+        </S.EditInfoContainer>
+      </S.Body>
+      <S.InputContainer>
+        <S.InputValue>휴대폰</S.InputValue>
+        <S.EditBox
+          onClick={() => {
+            const newPhone = prompt(`휴대전화`);
+            if (newPhone) {
+              setMyContact((prevContact) => ({
+                ...prevContact,
+                phone: newPhone,
+              }));
+            }
+          }}
+        >
+          <S.EditContact>{myContact.phone}</S.EditContact>
+          <S.pencilicon>
+            <img src={pencilicon} />
+          </S.pencilicon>
+        </S.EditBox>
+      </S.InputContainer>
+      <S.InputContainer>
+        <S.InputValue>이메일 주소</S.InputValue>
+        <S.EditBox
+          onClick={() => {
+            const newEmail = prompt(`이메일`);
+            if (newEmail) {
+              setMyContact((prevContact) => ({
+                ...prevContact,
+                email: newEmail,
+              }));
+            }
+          }}
+        >
+          <S.EditContact>{myContact.email}</S.EditContact>
+          <S.pencilicon>
+            <img src={pencilicon} />
+          </S.pencilicon>
+        </S.EditBox>
+      </S.InputContainer>
+      <S.InputContainer>
+        <S.InputValue>유선전화</S.InputValue>
+        <S.EditBox
+          onClick={() => {
+            const newTel = prompt(`유선전화`);
+            if (newTel) {
+              setMyContact((prevContact) => ({
+                ...prevContact,
+                tel: newTel,
+              }));
+            }
+          }}
+        >
+          <S.EditContact>{myContact.tel}</S.EditContact>
+          <S.pencilicon>
+            <img src={pencilicon} />
+          </S.pencilicon>
+        </S.EditBox>
+      </S.InputContainer>
+      <S.InputContainer>
+        <S.InputValue>주소</S.InputValue>
+        <S.EditBox
+          onClick={() => {
+            const newAddress = prompt(`주소`);
+            if (newAddress) {
+              setMyContact((prevContact) => ({
+                ...prevContact,
+                address: newAddress,
+              }));
+            }
+          }}
+        >
+          <S.EditContact>{myContact.address}</S.EditContact>
+          <S.pencilicon>
+            <img src={pencilicon} />
+          </S.pencilicon>
+        </S.EditBox>
+      </S.InputContainer>
     </>
   );
 }
--- src/pages/MyPageEdit/MyPageEdit.style.jsx
+++ src/pages/MyPageEdit/MyPageEdit.style.jsx
@@ -3,33 +3,158 @@ import styled from '@emotion/styled';
 export const Header = styled.div`
   width: 100%;
   height: 82px;
-  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 15px;
 `;
+
+export const arrowicon = styled.div`
+  width: 10px;
+  height: 9px;
+  display: flex;
+  align-items: center;
+`;
+
 export const welletlogo = styled.div`
-  width: 77.075px;
-  height: 21.24px;
-  flex-shrink: 0;
-  position: absolute;
-  top: 28px;
-  left: 50%;
-  transform: translateX(-50%);
+  width: 78px;
+  height: 22px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 export const EditIcon = styled.div`
   display: inline-flex;
   padding: 4px 10px;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 8px;
   border-radius: 10px;
   background: var(--color-1, #ebf3fe);
   color: var(--primary, #2d29ff);
   font-size: 9px;
-  font-style: normal;
   font-weight: 400;
-  line-height: 150%; /* 13.5px */
+  line-height: 150%;
   letter-spacing: -0.5px;
-  position: absolute;
-  top: 20px;
-  right: 20px;
   height: 24px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Body = styled.div`
+  width: calc(100% - 39px);
+  height: 100px;
+  display: flex;
+  align-items: center;
+  margin-left: 39px;
+`;
+
+export const PicContainer = styled.div`
+  width: 75px;
+  height: 75px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+export const ProfilePic = styled.div`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 68.239px;
+  background:
+    linear-gradient(
+      0deg,
+      rgba(45, 41, 255, 0.3) 0%,
+      rgba(45, 41, 255, 0.3) 100%
+    ),
+    url(<path-to-image>) lightgray 50% / cover no-repeat;
+  position: relative;
+`;
+
+export const EditInfoContainer = styled.div`
+  width: 200px;
+  height: 75px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 30px;
+`;
+export const EditName = styled.div`
+  color: var(--grey4, #1a1a1a);
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 150%;
+  letter-spacing: -0.5px;
+`;
+
+export const EditInfo = styled.div`
+  color: var(--grey4, #1a1a1a);
+  font-size: 11px;
+  font-weight: 400;
+  line-height: 150%;
+  letter-spacing: -0.5px;
+  display: flex;
+`;
+
+export const EditCompany = styled.div`
+  color: var(--grey1, #c8c8c8);
+  font-size: 11px;
+  font-weight: 400;
+  line-height: 150%;
+  letter-spacing: -0.5px;
+`;
+
+export const arrowdownicon = styled.div`
+  fill: var(--white, #fff);
+  stroke-width: 0.8px;
+  stroke: var(--grey4, #1a1a1a);
+`;
+export const EditBox = styled.div`
+  width: 690px;
+  height: 50px;
+  flex-shrink: 0;
+  border-radius: 10px;
+  background: #f4f4f9;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 15px;
+`;
+
+export const EditContact = styled.div`
+  color: var(--grey2, #949494);
+  font-size: 11px;
+  font-weight: 400;
+  line-height: 150%;
+  letter-spacing: -0.5px;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+  padding: 5px;
+`;
+
+export const InputValue = styled.div`
+  color: var(--grey3, #555);
+  font-size: 11px;
+  font-weight: 400;
+  line-height: 150%;
+  letter-spacing: -0.5px;
+  width: 690px;
+`;
+
+export const pencilicon = styled.div`
+  width: 13px;
+  height: 13px;
+  flex-shrink: 0;
+  opacity: 0.5;
+`;
+
+export const galleryicon = styled.div`
+  width: 18px;
+  height: 18px;
+  flex-shrink: 0;
+  position: absolute;
 `;
