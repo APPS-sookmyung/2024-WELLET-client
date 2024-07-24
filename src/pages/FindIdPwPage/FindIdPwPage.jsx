5d05fcc08aaddd91ffd77b3827a587009cda5397
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-07-24 20:57:28 +0900

    Feat: 로그인 페이지에서 이메일 로그인, 아이디 비번 찾기 페이지 라우팅

---
+++ src/pages/FindIdPwPage/FindIdPwPage.jsx
@@ -0,0 +1,5 @@
+import * as S from './FindIdPwPage.style';
+
+export default function FindIdPwPage() {
+  return <S.Container>id 및 비밀번호 찾기</S.Container>;
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,4 +10,8 @@ export { default as OnBoardingPage } from './OnBoardingPage';
 
 export { default as LoginPage } from './LoginPage';
 
+export { default as FindIdPwPage } from './FindIdPwPage';
+
+export { default as EmailLoginPage } from './EmailLoginPage';
+
 export { default as MyPageEdit } from './MyPageEdit';
