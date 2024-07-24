3cdccb4b28e0cd22a7dbc89fad95196aea2efee4
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-07-24 21:23:53 +0900

    Feat: 일반 로그인 페이지에서 이메일 로그인 페이지로 Link 추가

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -3,6 +3,7 @@ import Icon from '../../components/Icon/Icon';
 import login_background_circles from '../../assets/imgs/login_background_circles.svg';
 import login_background_lines from '../../assets/imgs/login_background_lines.svg';
 import { PrimaryButton, SecondaryButton } from '../../components';
+import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
   return (
@@ -27,7 +28,9 @@ export default function LoginPage() {
           </S.KakaoBtn>
           카카오 계정 로그인
         </PrimaryButton>
-        <SecondaryButton Link=''>이메일로 로그인</SecondaryButton>
+        <Link to='/login/email'>
+          <SecondaryButton>이메일로 로그인</SecondaryButton>
+        </Link>
         <S.SignUpOption>
           <p>계정이 없으신가요?</p>
           <p>회원가입</p>
