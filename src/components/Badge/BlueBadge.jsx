1b8801aac3066573f48acdea1bc1f7940b83fdf6
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-08-05 21:23:16 +0900

    Feat: BlueBadge 컴포넌트 커스텀 옵션 추가

--- src/components/Badge/BlueBadge.jsx
+++ src/components/Badge/BlueBadge.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import * as S from './BlueBadge.style';
 
-export default function BlueBadge({ badges, activeBadge, setActiveBadge }) {
+export default function BlueBadge({
+  badges,
+  activeBadge,
+  setActiveBadge,
+  xBtnClick,
+}) {
   return (
     <>
       {badges.map((badge) => (
         <S.BlueBadge
           key={badge.value}
-          isActive={activeBadge === badge.value}
-          onClick={() => setActiveBadge(badge.value)}
+          isActive={activeBadge ? activeBadge === badge.value : false}
+          onClick={() => setActiveBadge && setActiveBadge(badge.value)}
         >
+          {xBtnClick && (
+            <p
+              onClick={(e) => {
+                xBtnClick(badge.value);
+              }}
+            >
+              X
+            </p>
+          )}
           {badge.label}
         </S.BlueBadge>
       ))}
