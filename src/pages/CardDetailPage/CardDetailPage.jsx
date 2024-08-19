3e7a76a018a99c2d4f2167884962a67b4135f08c
kimhyerims <hyerim218@naver.com>
2024-08-19 23:36:22 +0900

    fix:충돌해결

--- src/pages/CardDetailPage/CardDetailPage.jsx
+++ src/pages/CardDetailPage/CardDetailPage.jsx
@@ -8,19 +8,6 @@ import CARDS_SAMPLE_DATA from '../../constants/cardsSampleData';
 
 export default function CardDetailPage() {
   const { id } = useParams();
-  const [activeBadge, setActiveBadge] = useState(null);
-  const [filteredBadges, setFilteredBadges] = useState([]);
-  const filteredData = CARDS_SAMPLE_DATA.filter(
-    (data) => data.name === decodeURIComponent(id)
-  );
-
-  useEffect(() => {
-    if (filteredData.length > 0) {
-      const category = filteredData[0].category;
-      const relevantBadges = badges.filter((badge) => badge.value === category);
-      setFilteredBadges(relevantBadges);
-    }
-  }, [filteredData]);
 
   const badges = [
     { label: '비즈니스', value: '비즈니스' },
@@ -29,82 +16,105 @@ export default function CardDetailPage() {
     { label: '대학교', value: '대학교' },
   ];
 
+  const filteredData = CARDS_SAMPLE_DATA.find(
+    (data) => data.name === decodeURIComponent(id)
+  );
+
+  const filteredBadges = filteredData
+    ? badges.filter((badge) => badge.value === filteredData.category)
+    : [];
+
+  const data = filteredData || {
+    imageUrl: '',
+    name: '이름없음',
+    job: '직책없음',
+    company: '회사없음',
+    phone: '전화번호없음',
+    email: '이메일없음',
+    tel: '유선전화없음',
+    address: '주소없음',
+    memo: '메모없음',
+  };
+
+  const profileImageUrl = data.imageUrl || ProfileImgDefault;
+
+  const [activeBadge, setActiveBadge] = useState(null);
+
   return (
     <>
-      {filteredData.map((data, index) => (
-        <React.Fragment key={index}>
-          <S.TopContainer>
-            <S.TopBar>
-              <S.LeftContainer1 />
-              <S.MyName>
-                <Icon id='logo-white' width='60' height='24.5' />
-              </S.MyName>
-              <Link to={`/card/${id}/edit`}>
-                <S.EditIcon>편집하기</S.EditIcon>
-              </Link>
-            </S.TopBar>
-            <S.MidBar>
-              <S.PicContainer>
-                <S.ProfilePic
-                  src={data.imageUrl || ProfileImgDefault}
-                  alt={`${data.name} 프로필`}
-                />
-              </S.PicContainer>
-            </S.MidBar>
-            <S.BotBar>
-              <S.NameBox>
-                <S.NameFont>{data.name}</S.NameFont>
-                <S.JobTeamFont>{data.job}</S.JobTeamFont>
-                <S.ComFont>{data.company}</S.ComFont>
-              </S.NameBox>
-              <S.SubBar />
-            </S.BotBar>
-          </S.TopContainer>
+      <S.TopContainer>
+        <S.TopBar>
+          <S.LeftContainer1 />
+          <S.MyName>
+            <Icon id='logo-white' width='60' height='24.5' />
+          </S.MyName>
+          <Link to={`/card/${id}/edit`}>
+            <S.EditIcon>편집하기</S.EditIcon>
+          </Link>
+        </S.TopBar>
+        <S.MidBar>
+          <S.PicContainer>
+            <S.ProfilePic src={profileImageUrl} alt={`${data.name} 프로필`} />
+          </S.PicContainer>
+        </S.MidBar>
+        <S.BotBar>
+          <S.NameBox>
+            <S.NameFont>{data.name}</S.NameFont>
+            <S.JobTeamFont>{data.job}</S.JobTeamFont>
+            <S.ComFont>{data.company}</S.ComFont>
+          </S.NameBox>
+          <S.SubBar />
+        </S.BotBar>
+      </S.TopContainer>
 
-          <S.BottomContainer>
-            <S.ConBar>연락처</S.ConBar>
-            <S.ContactContainer>
-              <S.InfoBox>
-                <S.UserInfoLabel>휴대폰</S.UserInfoLabel>
-                <S.ContactWrapper>
-                  <S.UserInfoValue>{data.phone}</S.UserInfoValue>
-                  <S.IconBox>
-                    <Icon id='message' width='20' height='14' />
-                    <Icon id='call' width='20' height='14' />
-                  </S.IconBox>
-                </S.ContactWrapper>
-              </S.InfoBox>
-              <S.InfoBox>
-                <S.UserInfoLabel>이메일</S.UserInfoLabel>
-                <S.ContactWrapper>
-                  <S.UserInfoValue>{data.email}</S.UserInfoValue>
-                  <Icon id='mail' width='20' height='14' />
-                </S.ContactWrapper>
-              </S.InfoBox>
-              <S.InfoBox>
-                <S.UserInfoLabel>유선전화</S.UserInfoLabel>
-                <S.ContactWrapper>
-                  <S.UserInfoValue>{data.tel}</S.UserInfoValue>
-                  <Icon id='call' width='20' height='14' />
-                </S.ContactWrapper>
-              </S.InfoBox>
-              <S.InfoBox>
-                <S.UserInfoLabel>주소</S.UserInfoLabel>
-                <S.UserInfoValue>{data.address}</S.UserInfoValue>
-              </S.InfoBox>
-            </S.ContactContainer>
-            <S.GroupButtonBar>그룹</S.GroupButtonBar>
-            <S.GroupButtonBox>
-              <BlueBadge
-                badges={filteredBadges}
-                activeBadge={activeBadge}
-                setActiveBadge={setActiveBadge}
-                fill='#2d29ff'
-              />
-            </S.GroupButtonBox>
-          </S.BottomContainer>
-        </React.Fragment>
-      ))}
+      <S.BottomContainer>
+        <S.ConBar>연락처</S.ConBar>
+        <S.ContactContainer>
+          <S.InfoBox>
+            <S.UserInfoLabel>휴대폰</S.UserInfoLabel>
+            <S.ContactWrapper>
+              <S.UserInfoValue>{data.phone}</S.UserInfoValue>
+              <S.IconBox>
+                <Icon id='message' width='20' height='14' />
+                <Icon id='call' width='20' height='14' />
+              </S.IconBox>
+            </S.ContactWrapper>
+          </S.InfoBox>
+          <S.InfoBox>
+            <S.UserInfoLabel>이메일</S.UserInfoLabel>
+            <S.ContactWrapper>
+              <S.UserInfoValue>{data.email}</S.UserInfoValue>
+              <Icon id='mail' width='20' height='14' />
+            </S.ContactWrapper>
+          </S.InfoBox>
+          <S.InfoBox>
+            <S.UserInfoLabel>유선전화</S.UserInfoLabel>
+            <S.ContactWrapper>
+              <S.UserInfoValue>{data.tel}</S.UserInfoValue>
+              <Icon id='call' width='20' height='14' />
+            </S.ContactWrapper>
+          </S.InfoBox>
+          <S.InfoBox>
+            <S.UserInfoLabel>주소</S.UserInfoLabel>
+            <S.UserInfoValue>{data.address}</S.UserInfoValue>
+          </S.InfoBox>
+        </S.ContactContainer>
+        <S.ConBar>메모</S.ConBar>
+        <S.ContactContainer>
+          <S.InfoBox>
+            <S.UserInfoValue>{data.memo}</S.UserInfoValue>
+          </S.InfoBox>
+        </S.ContactContainer>
+        <S.GroupButtonBar>그룹</S.GroupButtonBar>
+        <S.GroupButtonBox>
+          <BlueBadge
+            badges={filteredBadges}
+            activeBadge={activeBadge}
+            setActiveBadge={setActiveBadge}
+            fill='#2d29ff'
+          />
+        </S.GroupButtonBox>
+      </S.BottomContainer>
     </>
   );
 }
--- src/pages/DetailEditPage/DetailEditPage.style.jsx
+++ src/pages/DetailEditPage/DetailEditPage.style.jsx
@@ -2,17 +2,18 @@ import styled from '@emotion/styled';
 
 export const Header = styled.div`
   width: 100%;
-  height: 82px;
+  height: 84px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 0 20px;
 `;
 
-export const Arrowicon = styled.div`
+export const Arrowicon = styled.button`
   width: 60px;
   display: flex;
   align-items: center;
+  cursor: pointer;
 `;
 
 export const Welletlogo = styled.div`
@@ -29,12 +30,12 @@ export const EditIconBox = styled.div`
   justify-content: end;
 `;
 
-export const EditIcon = styled.div`
+export const EditIcon = styled.button`
   padding: 4px 10px;
   border-radius: 10px;
   background: var(--color-1, #ebf3fe);
   color: var(--primary, #2d29ff);
-  font-size: 9px;
+  font-size: 11px;
   font-weight: 400;
   line-height: 150%;
   letter-spacing: -0.5px;
@@ -71,13 +72,14 @@ export const ProfilePic = styled.div`
   position: relative;
 `;
 
-export const Galleryicon = styled.div`
+export const Galleryicon = styled.button`
   width: 100%;
   height: 100%;
   position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
+  cursor: pointer;
 `;
 
 export const EditGuide = styled.div`
@@ -96,6 +98,15 @@ export const EditInfoContainer = styled.div`
   justify-content: center;
 `;
 
+export const InputNameBox = styled.input`
+  background-color: transparent;
+  border: none;
+  outline: none;
+  color: var(--grey2, #949494);
+  font-size: 16px;
+  font-weight: 600;
+`;
+
 export const Name = styled.div`
   color: var(--grey4, #1a1a1a);
   font-size: 16px;
@@ -111,11 +122,12 @@ export const EditName = styled.div`
   align-items: center;
 `;
 
-export const PencilIcon = styled.div`
+export const PencilIcon = styled.button`
   width: 16px;
   height: 16px;
   display: flex;
   align-items: center;
+  cursor: pointer;
 `;
 
 export const EditInfo = styled.div`
@@ -143,6 +155,7 @@ export const InputWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 2px;
+  padding: 5px 0;
 `;
 
 export const InputLabel = styled.label`
@@ -174,6 +187,9 @@ export const Input = styled.input`
   line-height: 140%;
   letter-spacing: -0.7px;
   outline: none;
+  &:focus {
+    color: #000;
+  }
 `;
 
 export const IconWrapper = styled.div`
