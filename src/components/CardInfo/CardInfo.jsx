813021be86ad4b14261bd3497e863223652c8dce
misung.dev <misung.dev@gmail.com>
2024-08-19 19:04:16 +0900

    Modify: 삭제모드 일때 명함 상세보기 페이지로 이동하는 오류 수정

--- src/components/CardInfo/CardInfo.jsx
+++ src/components/CardInfo/CardInfo.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import * as S from './CardInfo.style';
 import Icon from '../../components/Icon/Icon';
@@ -12,38 +13,42 @@ export default function CardInfo({
   isSelected = false,
   onClick = () => {},
 }) {
-  return (
-    <Link to={`/card/${name}`}>
-      <S.Card
-        onClick={onClick}
-        isDeleteMode={isDeleteMode}
-        isSelected={isSelected}
-      >
-        <S.CardWrapper>
-          {imageUrl ? (
-            <img src={imageUrl} alt={`${name} 프로필`} />
-          ) : (
-            <Icon id='profile-basic' fill='none' />
-          )}
-          <S.Info>
-            <S.Name isSelected={isSelected}>{name}</S.Name>
-            <S.Job isSelected={isSelected}>
-              {job}, {company}
-            </S.Job>
-          </S.Info>
-        </S.CardWrapper>
-        {!isDeleteMode && (
-          <S.ArrowIconWrapper>
-            <Icon id='arrow-right' fill='none' />
-          </S.ArrowIconWrapper>
+  const cardContent = (
+    <S.Card
+      onClick={onClick}
+      isDeleteMode={isDeleteMode}
+      isSelected={isSelected}
+    >
+      <S.CardWrapper>
+        {imageUrl ? (
+          <img src={imageUrl} alt={`${name} 프로필`} />
+        ) : (
+          <Icon id='profile-basic' fill='none' />
         )}
-        {isDeleteMode && (
-          <S.ArrowIconWrapper>
-            <Icon id={isSelected ? 'circle-check' : 'circle'} fill='none' />
-          </S.ArrowIconWrapper>
-        )}
-      </S.Card>
-    </Link>
+        <S.Info>
+          <S.Name isSelected={isSelected}>{name}</S.Name>
+          <S.Job isSelected={isSelected}>
+            {job}, {company}
+          </S.Job>
+        </S.Info>
+      </S.CardWrapper>
+      {!isDeleteMode && (
+        <S.ArrowIconWrapper>
+          <Icon id='arrow-right' fill='none' />
+        </S.ArrowIconWrapper>
+      )}
+      {isDeleteMode && (
+        <S.ArrowIconWrapper>
+          <Icon id={isSelected ? 'circle-check' : 'circle'} fill='none' />
+        </S.ArrowIconWrapper>
+      )}
+    </S.Card>
+  );
+
+  return isDeleteMode ? (
+    cardContent
+  ) : (
+    <Link to={`/card/${name}`}>{cardContent}</Link>
   );
 }
 
--- src/pages/CardDetailPage/CardDetailPage.jsx
+++ src/pages/CardDetailPage/CardDetailPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import * as S from './CardDetailPage.style';
 import Icon from '../../components/Icon/Icon';
