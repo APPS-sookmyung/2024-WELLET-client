7222040e4955202ea34916b99584e37f205b2b00
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-08-19 22:26:56 +0900

    Remove: 홈 페이지의 필터 옵션, 그룹 수정 버튼 삭제

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,7 @@
 import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as S from './HomePage.style';
-import {
-  Header,
-  SearchBar,
-  CardInfo,
-  MyCard,
-  Icon,
-  AddGroupModal,
-} from '../../components';
+import { Header, SearchBar, CardInfo, MyCard } from '../../components';
 import MY_CARD_SAMPLE_DATA from '../../constants/myCardSampleData';
 import { useVisibleCardsEffect } from '../../utils/HomePageUtils/homePageEffects';
 import CARDS_SAMPLE_DATA from '../../constants/cardsSampleData.js';
@@ -16,85 +9,63 @@ import CARDS_SAMPLE_DATA from '../../constants/cardsSampleData.js';
 export default function HomePage() {
   const navigate = useNavigate();
   const [filterdList, setFilterdList] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useVisibleCardsEffect(setFilterdList, CARDS_SAMPLE_DATA);
 
   return (
-    <>
-      <S.HomePage>
-        <S.Top>
-          <S.Padding>
-            <Header color='white' />
-            <SearchBar theme='blue' />
-            <S.CardListTitle>
-              <p>내 명함</p>
-              <p>나의 명함 확인 및 관리하기</p>
-            </S.CardListTitle>
-          </S.Padding>
-          <S.MyCardContainer>
-            <MyCard
-              backgroundColor='#fff'
-              name={MY_CARD_SAMPLE_DATA.name}
-              job={MY_CARD_SAMPLE_DATA.job}
-              company={MY_CARD_SAMPLE_DATA.company}
-              imageUrl={MY_CARD_SAMPLE_DATA.imageUrl}
-              tel={MY_CARD_SAMPLE_DATA.tel}
-              email={MY_CARD_SAMPLE_DATA.email}
-              address={MY_CARD_SAMPLE_DATA.address}
-              onClick={() => navigate('/mypage')}
-            />
-          </S.MyCardContainer>
-          <S.UpDownBarBox>
-            <S.UpDownBar />
-          </S.UpDownBarBox>
-        </S.Top>
-        <S.Container>
+    <S.HomePage>
+      <S.Top>
+        <S.Padding>
+          <Header color='white' />
+          <SearchBar theme='blue' />
           <S.CardListTitle>
-            <p style={{ color: '#000' }}>둘러보기</p>
-            <p style={{ color: '#555' }}>등록된 명함 찾아보기</p>
+            <p>내 명함</p>
+            <p>나의 명함 확인 및 관리하기</p>
           </S.CardListTitle>
-          <S.ListOpiton>
-            <S.ArrowBottomIcon>
-              <Icon id='arrow-bottom' />
-            </S.ArrowBottomIcon>
-            <S.Filter>최근 등록 순</S.Filter>
-            <S.AddGroup onClick={() => setIsModalOpen(true)}>
+        </S.Padding>
+        <S.MyCardContainer>
+          <MyCard
+            backgroundColor='#fff'
+            name={MY_CARD_SAMPLE_DATA.name}
+            job={MY_CARD_SAMPLE_DATA.job}
+            company={MY_CARD_SAMPLE_DATA.company}
+            imageUrl={MY_CARD_SAMPLE_DATA.imageUrl}
+            tel={MY_CARD_SAMPLE_DATA.tel}
+            email={MY_CARD_SAMPLE_DATA.email}
+            address={MY_CARD_SAMPLE_DATA.address}
+            onClick={() => navigate('/mypage')}
+          />
+        </S.MyCardContainer>
+        <S.UpDownBarBox>
+          <S.UpDownBar />
+        </S.UpDownBarBox>
+      </S.Top>
+      <S.Container>
+        <S.CardListTitle>
+          <p style={{ color: '#000' }}>둘러보기</p>
+          <p style={{ color: '#555' }}>등록된 명함 찾아보기</p>
+        </S.CardListTitle>
+        {filterdList.length > 0 && (
+          <>
+            <S.CardContainer>
+              {filterdList.map((data, index) => (
+                <CardInfo
+                  key={index}
+                  name={data.name}
+                  job={data.job}
+                  company={data.company}
+                  imageUrl={data.imageUrl}
+                />
+              ))}
+            </S.CardContainer>
+            <S.BottomMoreBtn>
               <S.EditBtnWrapper>
-                <S.EditText>그룹 수정</S.EditText>
-                <S.MoreIcon>
-                  <Icon id='more' fill='#2D29FF' />
-                </S.MoreIcon>
+                <S.EditText>더 보기</S.EditText>
               </S.EditBtnWrapper>
-            </S.AddGroup>
-          </S.ListOpiton>
-          {filterdList.length > 0 && (
-            <>
-              <S.CardContainer>
-                {filterdList.map((data, index) => (
-                  <CardInfo
-                    key={index}
-                    name={data.name}
-                    job={data.job}
-                    company={data.company}
-                    imageUrl={data.imageUrl}
-                  />
-                ))}
-              </S.CardContainer>
-              <S.BottomMoreBtn>
-                <S.EditBtnWrapper>
-                  <S.EditText>더 보기</S.EditText>
-                </S.EditBtnWrapper>
-              </S.BottomMoreBtn>
-            </>
-          )}
-        </S.Container>
-      </S.HomePage>
-      <AddGroupModal
-        isModalOpen={isModalOpen}
-        setIsModalOpen={setIsModalOpen}
-        style={{ visibility: isModalOpen ? 'visible' : 'hidden' }}
-      />
-    </>
+            </S.BottomMoreBtn>
+          </>
+        )}
+      </S.Container>
+    </S.HomePage>
   );
 }
