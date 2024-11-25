5211f2a7d6ab9b0e2365a952da40f20a91bc4e69
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-11-26 01:52:25 +0900

    #104 [FEAT] 로그인 구현 전까지 수동으로 토큰 넣을 수 있도록 dummyToken 설정

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,9 +1,36 @@
 import axios from 'axios';
 
+const VITE_SERVER_DOMAIN = import.meta.env.VITE_SERVER_DOMAIN;
+
 export const authAxios = axios.create({
-  baseURL: 'http://43.203.244.152:8080',
+  baseURL: VITE_SERVER_DOMAIN,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
+const getTokenFromCookie = () => {
+  // const cookie = document.cookie
+  //   .split('; ')
+  //   .find((row) => row.startsWith('token='));
+  // return cookie ? cookie.split('=')[1] : null;
+  return dummyToken;
+};
+
+const dummyToken =
+  '여기에 로그인 링크 타고 들어갔을 때 쿠키에 뜨는 토큰값을 넣어주세요';
+
+// 요청을 보낼 때 Bearer Token을 Authorization 헤더에 추가합니다.
+authAxios.interceptors.request.use(
+  (config) => {
+    const token = getTokenFromCookie();
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
