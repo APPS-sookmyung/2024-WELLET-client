0ffd23b419c29d22bd6dd71919aefb6c7fc5bce7
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-12-22 17:05:11 +0900

    #110 [FEAT] 카드 검색

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import * as S from './SearchBar.style';
 import Icon from '../../components/Icon/Icon';
+import { searchCards } from '../../apis/cards.js';
 
 export default function SearchBar({ theme }) {
+  const [keyword, setKeyword] = useState('');
+
+  const handleSearch = () => {
+    if (keyword.trim() === '') return;
+    searchCards(keyword);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
   const iconId =
     theme === 'white' ? 'magnifying-glass-gray2' : 'magnifying-glass-gray1';
   const placeholderText =
@@ -16,11 +29,13 @@ export default function SearchBar({ theme }) {
       <S.SearchIcon theme={theme}>
         <Icon id={iconId} fill='none' />
       </S.SearchIcon>
-      <S.SearchInput theme={theme} placeholder={placeholderText} />
+      <S.SearchInput
+        theme={theme}
+        placeholder={placeholderText}
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
     </S.SearchBar>
   );
 }
-
-SearchBar.propTypes = {
-  theme: PropTypes.oneOf(['white', 'blue']).isRequired,
-};
